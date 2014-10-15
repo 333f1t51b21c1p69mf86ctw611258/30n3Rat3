25cd419dbbacb3829811e4ddbb8917ddfa13792d
f1t51b21c1p69mf86ctw611258 <nguyenth86@icloud.com>
2014-10-15 16:14:46 +0700

    add dictionary file

---
+++ DB/_FullScript_/vnp_common/__DICTIONARY/RATING_TYPE.sql
@@ -0,0 +1,10 @@
+﻿SET DEFINE OFF;
+Insert into VNP_COMMON.RATING_TYPE
+   (RATING_TYPE_ID, TYPE_NAME, UNBILL)
+ Values
+   (1, 'Thuê bao thường', '0');
+Insert into VNP_COMMON.RATING_TYPE
+   (RATING_TYPE_ID, TYPE_NAME, UNBILL)
+ Values
+   (2, 'Thuê bao VNP', '1');
+COMMIT;
