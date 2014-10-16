7f6375cf5ca1b4f0d52e4d0f1b3cb80963bba54f
f1t51b21c1p68mf86ctw811256 <nguyenth86@me.com>
2014-10-16 10:14:20 +0700

    add some file

---
+++ DB/_FullScript_/vnp_common/__DICTIONARY/ELCRATE_CMD.sql
@@ -0,0 +1,10 @@
+﻿SET DEFINE OFF;
+Insert into VNP_COMMON.ELCRATE_CMD
+   (CMD_CODE, COMMAND)
+ Values
+   (10, 'Framework:Shutdown=true');
+Insert into VNP_COMMON.ELCRATE_CMD
+   (CMD_CODE, COMMAND)
+ Values
+   (20, 'CacheFactory:Reload=true');
+COMMIT;
---
+++ DB/_FullScript_/vnp_common/__DICTIONARY/RC_TARIFF_TYPE.sql
@@ -0,0 +1,18 @@
+﻿SET DEFINE OFF;
+Insert into VNP_COMMON.RC_TARIFF_TYPE
+   (RC_TARIFF_TYPE_ID, TYPE_NAME)
+ Values
+   (1, 'Chia theo số ngày trong tháng');
+Insert into VNP_COMMON.RC_TARIFF_TYPE
+   (RC_TARIFF_TYPE_ID, TYPE_NAME)
+ Values
+   (2, 'Chia theo số ngày cố định');
+Insert into VNP_COMMON.RC_TARIFF_TYPE
+   (RC_TARIFF_TYPE_ID, TYPE_NAME)
+ Values
+   (3, 'Chia theo tỷ lệ %');
+Insert into VNP_COMMON.RC_TARIFF_TYPE
+   (RC_TARIFF_TYPE_ID, TYPE_NAME)
+ Values
+   (4, 'Gán cứng mức tiền');
+COMMIT;
